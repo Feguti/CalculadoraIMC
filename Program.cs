1fa5b309e372402069c70a4631bd22b21f85662a
Felipe Gutierrez <feguti4@gmail.com>
2022-03-29 14:32:43 -0300

    Adicionando as funcionalidades de perguntar o nome, altura e peso e calcular o IMC e exibir o resultado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fala tu parça, qual seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("\nMe diz a sua altura, em metros de preferência. Ex: 1,70");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("\nQuanto você pesa? Ex: 70,3");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+
+            Console.WriteLine("\n{0} sua altura é {1} e seu peso é {2}", nome, altura, peso);
+            Console.WriteLine("Seu IMC é {0}", Math.Round(imc));
         }
     }
 }
