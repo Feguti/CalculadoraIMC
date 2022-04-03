ea044d486642769b646d4bbc6591831c8624176c
Felipe Gutierrez <feguti4@gmail.com>
2022-04-03 15:44:04 -0300

    Adicionando funcionalidade que indica quanto peso o usuário precisa ganhar ou perder para atingir um peso normal

--- Program.cs
+++ Program.cs
@@ -17,38 +17,53 @@ namespace CalculadoraIMC
 
             double imc = Math.Round(peso / (altura * altura), 2);
 
-            Console.WriteLine("\n{0} sua altura é {1} e seu peso é {2}", nome, altura, peso);
+            Console.WriteLine("\n{0} sua altura é {1}m e seu peso é {2}Kg", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
 
             if(imc < 17)
             {
-                Console.WriteLine("Você está muito abaixo do peso");
+                Console.WriteLine("Você está muito abaixo do peso\n");
             }
-            else if(imc >= 17 || imc < 18.50)
+            else if(imc >= 17 && imc < 18.50)
             {
-                Console.WriteLine("Você está abaixo do peso");
+                Console.WriteLine("Você está abaixo do peso\n");
             }
-            else if(imc >= 18.50 || imc < 25)
+            else if(imc >= 18.50 && imc < 25)
             {
-                Console.WriteLine("Peso normal");
+                Console.WriteLine("Peso normal\n");
             }
-            else if(imc >= 25 || imc < 30)
+            else if(imc >= 25 && imc < 30)
             {
-                Console.WriteLine("Acima do peso");
+                Console.WriteLine("Você está acima do peso\n");
             }
-            else if(imc >= 30 || imc < 35)
+            else if(imc >= 30 && imc < 35)
             {
-                Console.WriteLine("Obesidade I");
+                Console.WriteLine("Obesidade I\n");
             }
-            else if(imc >= 35 || imc < 40)
+            else if(imc >= 35 && imc < 40)
             {
-                Console.WriteLine("Obesidade II (Severa)");
+                Console.WriteLine("Obesidade II (Severa)\n");
             }
-            else if(imc > 40)
+            else
             {
-                Console.WriteLine("Obesidade III (Mórbida)");
+                Console.WriteLine("Obesidade III (Mórbida)\n");
             }
 
+            if(imc < 18.50)
+            {
+                double peso_ideal = 18.50 * (altura * altura);
+                double peso_necessario = Math.Round(peso_ideal - peso, 2);
+
+                Console.WriteLine("Você precisa ganhar {0}Kg", peso_necessario);
+            }
+
+            else if(imc >= 25)
+            {
+                double peso_ideal = 24.99 * (altura * altura);
+                double peso_necessario = Math.Round(peso - peso_ideal, 2);
+
+                Console.WriteLine("Você precisa perder {0}Kg", peso_necessario);
+            }
         }
     }
 }
